a2361e19d6c2e2ad76f3a1833924dfb8c1ec15d6
varerysan <valery.mosyagin@gmail.com>
2018-10-27 18:40:33 +0300

    Add example with car

---
+++ some_ideas/speed.py
@@ -0,0 +1,5 @@
+dist = float(input("Введите расстояние между городами:"))
+speed = float(input("Введите скорость автомобиля:"))
+time = dist/speed
+print("Автомобиль доедет за", time, "ч")
+print("Автомобиль доедет за {:.2f} ч.".format(time))
