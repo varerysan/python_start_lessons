55513cdd783c8376bb42f125ac7edcbb733cf53f
varerysan <valery.mosyagin@gmail.com>
2018-10-27 19:12:32 +0300

    Add tasks with divide number to tens and units

---
+++ some_ideas/tens_and_units_v2.py
@@ -0,0 +1,15 @@
+# Разбиение двузначного числа на десятки и единицв
+
+while True:
+    number = int(input("Введите двузначное число:"))
+        
+    if number < 10 or number > 99:
+        print("Это не двузначное число. Повторите ввод.")
+    else:
+        break
+
+ed = number % 10
+des = number // 10
+
+print("Десятков:", des)
+print("Единиц:", ed)
