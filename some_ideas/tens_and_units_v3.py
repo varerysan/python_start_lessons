9345cafe08aa900add17f4389f8588c72be458c5
varerysan <valery.mosyagin@gmail.com>
2018-10-28 23:25:11 +0300

    Thousand, tens, units

---
+++ some_ideas/tens_and_units_v3.py
@@ -0,0 +1,17 @@
+# Разбиение трёхзначного числа на сотни, десятки и единицы
+
+while True:
+    number = int(input("Введите трёхзначное число:"))
+        
+    if number < 100 or number > 999:
+        print("Это не трёхзначное число. Повторите ввод.")
+    else:
+        break
+
+sot = number // 100
+ed = number % 10
+des = (number % 100) // 10
+
+print("Сотен:", sot)
+print("Десятков:", des)
+print("Единиц:", ed)
