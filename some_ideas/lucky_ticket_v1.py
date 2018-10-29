25dc55b17c5652f636ed57a9dc45658524864ccc
varerysan <valery.mosyagin@gmail.com>
2018-10-29 23:22:48 +0300

    Add test for lucky ticket

--- some_ideas/lucky_ticket_v1.py
+++ some_ideas/lucky_ticket_v1.py
@@ -11,4 +11,12 @@ d4 = (ticket // 1000) % 10
 d5 = (ticket // 10000) % 10
 d6 = (ticket // 100000) % 10
 
-print()
+print("Ticket: {}{}{}{}{}{}".format(d6, d5, d4, d3, d2, d1))
+
+sum1 = d1 + d2 + d3
+sum2 = d4 + d5 + d6
+
+if sum1 == sum2:
+    print("Счастливый билет")
+else:
+    print("Обычный билет")
