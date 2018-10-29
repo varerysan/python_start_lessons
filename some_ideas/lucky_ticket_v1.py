bb0f8fecaf470e2cd5ee22c23be71a2834c722c9
varerysan <valery.mosyagin@gmail.com>
2018-10-29 23:16:52 +0300

    Add some to ticket test

--- some_ideas/lucky_ticket_v1.py
+++ some_ideas/lucky_ticket_v1.py
@@ -3,3 +3,12 @@
 ticket = int(input("Введите номер билета:"))
 
 print("ticket={}=".format(ticket))
+
+d1 = ticket % 10
+d2 = (ticket // 10) % 10
+d3 = (ticket // 100) % 10
+d4 = (ticket // 1000) % 10
+d5 = (ticket // 10000) % 10
+d6 = (ticket // 100000) % 10
+
+print()
