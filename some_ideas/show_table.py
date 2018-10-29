c1eeb5b57640a635348ce0ebfb73cf57b5787c70
varerysan <valery.mosyagin@gmail.com>
2018-10-30 01:26:16 +0300

    Table with pseudographics by UTF8 codes

---
+++ some_ideas/show_table.py
@@ -0,0 +1,19 @@
+# Показ красивой таблицы с ценами
+
+def print_line(width1, width2):
+    print("+" + 
+          "-" * width1 + 
+          "+" +
+          "-" * width2 +
+          "+")    
+
+def show_table(table, width1, width2):
+    print_line(width1, width2)
+    for name, price in table.items():
+        print("|{}|{}|".format( name.ljust(width1), str(price).rjust(width2) ))
+    print_line(width1, width2)
+
+
+menu = {"Картошка":25, "Суп": 17}
+
+show_table(menu,10,3)
---
+++ some_ideas/show_table_v2.py
@@ -0,0 +1,37 @@
+# Показ красивой таблицы с ценами
+
+
+sym_V = "\u2503"
+sym_H = "\u2501"
+sym_LT = "\u250F"
+sym_RT = "\u2513"
+sym_LB = "\u2517"
+sym_RB = "\u251b"
+sym_MT = "\u2533"
+sym_MB = "\u253B"
+
+
+def print_line_top(width1, width2):
+    print(sym_LT + 
+          sym_H * width1 + 
+          sym_MT +
+          sym_H * width2 +
+          sym_RT)    
+
+def print_line_bottom(width1, width2):
+    print(sym_LB + 
+          sym_H * width1 + 
+          sym_MB +
+          sym_H * width2 +
+          sym_RB)
+
+def show_table(table, width1, width2):
+    print_line_top(width1, width2)
+    for name, price in table.items():
+        print("{}{}{}{}{}".format(sym_V, name.ljust(width1), sym_V, str(price).rjust(width2), sym_V ))
+    print_line_bottom(width1, width2)
+
+
+menu = {"Картошка":25, "Суп": 17}
+
+show_table(menu,10,3)
