62051c0acb60ea4b1ac791112fa4583377a9ea33
varerysan <valery.mosyagin@gmail.com>
2018-10-30 00:44:42 +0300

    Python way v2

--- some_ideas/lucky_ticket_v7.py
+++ some_ideas/lucky_ticket_v7.py
@@ -1,11 +1,9 @@
 # Проверка счастливый билет или нет
 
 all_sums = {}
-for n in range(1000):
-    d1 = n % 10
-    d2 = (n // 10) % 10
-    d3 = n // 100
-    part_sum = d1 + d2 + d3    
+for n in range(1000):    
+    line = (str(n)).zfill(3)    
+    part_sum = sum([int(s) for s in line])  
     all_sums[part_sum] = all_sums.get(part_sum, 0) + 1
 
 total = sum([n*n for n in all_sums.values() ])
