74e13abfc1c4ca3969441d64d7d96863243f4908
varerysan <valery.mosyagin@gmail.com>
2018-10-29 23:29:34 +0300

    Second lucky ticket variant

---
+++ some_ideas/lucky_ticket_v2.py
@@ -0,0 +1,21 @@
+# Проверка счастливый билет или нет
+
+def is_lucky(ticket):
+    d1 = ticket % 10
+    d2 = (ticket // 10) % 10
+    d3 = (ticket // 100) % 10
+    d4 = (ticket // 1000) % 10
+    d5 = (ticket // 10000) % 10
+    d6 = (ticket // 100000) % 10
+    
+    return d1 + d2 + d3 == d4 + d5 + d6        
+    
+    
+ticket = int(input("Введите номер билета:"))
+
+if is_lucky(ticket):
+    print("Счастливый билет")
+else:
+    print("Обычный билет")    
+
+
