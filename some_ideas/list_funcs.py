329abf7ae4f0c0f08abe6082475d1993a71646fc
varerysan <valery.mosyagin@gmail.com>
2018-10-31 23:01:21 +0300

    List funcs

--- some_ideas/list_funcs.py
+++ some_ideas/list_funcs.py
@@ -4,4 +4,24 @@ for value in spisok:
     print("Value={}".format(value))
     
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
 numbers2 = []
+for n in range(10):
+    numbers2.append(n)
+    
+print("Numbers2=", numbers2)
+
+numbers3 = []
+for n in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]:
+    numbers3.append(n)
+print("Numbers3=", numbers3)
+
+numbers4 = list(range(10))
+print("Numbers4=", numbers4)
+
+two_lists = list(range(100,105)) + list(range(200,205))
+print("two_lists=", two_lists)
+
+
+
+    
