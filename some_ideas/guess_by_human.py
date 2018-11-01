1f703971265207f6de917c57a67e817cb74f6191
varerysan <valery.mosyagin@gmail.com>
2018-11-02 01:42:47 +0300

    Guess game by human

---
+++ some_ideas/guess_by_human.py
@@ -0,0 +1,16 @@
+# Игра. Компьютер задусывет число, человек угадывает
+import random
+
+comp = random.randint(1,100)
+
+print("Попробуйти угадать число от 1 до 100.")
+
+while True:
+    human = int(input("Введите число:"))
+    if comp > human:
+        print("Больше")
+    elif comp < human:
+        print("Меньше")
+    else:
+        print("Верно! Вы угадали.")
+        break
