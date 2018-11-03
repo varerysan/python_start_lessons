170f34616af098a25045473a890c42323b375b08
varerysan <valery.mosyagin@gmail.com>
2018-11-03 13:04:55 +0300

    Find max value in list

---
+++ some_ideas/find_max_v1.py
@@ -0,0 +1,14 @@
+# плгоритм поиска максимального элемента в списке, учитывая,
+# что список может быть пуст
+
+data = [10,234,34,567,534,17]
+
+
+max_val = None
+if len(data) > 0:
+    max_val = data[0]
+    for val in data[1:]:
+        if val > max_val:
+            max_val = val
+
+print("Max=",max_val)
