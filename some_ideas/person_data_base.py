dd69e94fe97c81d56097497c54fe3ca2893f9a58
varerysan <valery.mosyagin@gmail.com>
2018-11-05 22:54:34 +0300

    Add write db

--- some_ideas/person_data_base.py
+++ some_ideas/person_data_base.py
@@ -1,14 +1,57 @@
 # простой пример небольшой базы даннх людей
 
 class Person:
-    def __init__(self):
-        self._name = ""
-        self._second_name = ""
-        self._birth_year = None
+    def __init__(self, name, surname, birth):
+        self._name = name
+        self._surname = surname
+        self._birth_year = birth
         
     def print(self):
         print("Name:", self._name)
-        print("Second name:", self._second_name)
+        print("Surname:", self._surname)
         print("Birth year:", self._birth_year)
         
+    def write(self, file):
+        file.write(self._name + "\n")
+        file.write(self._surname + "\n")
+        file.write(str(self._birth_year) + "\n")
+        
+        
+
+
+# Cosmos 3M
+# лапота. Энергия
+
+class Base:
+    def __init__(self):
+        self._persons = []
+        self._file_name = "db_persons.dat"
     
+    def print(self):
+        for p in self._persons:
+            p.print()
+            
+    def add(self, person):
+        self._persons.append(person)
+        
+    def write(self):
+        with open(self._file_name,"w") as file:
+            for p in self._persons:
+                p.write(file)
+        
+
+base = Base()
+
+p = Person("John", "Donny", 1995)
+base.add(p)
+
+p = Person("Aaa", "Bbbbb", 1990)
+base.add(p)
+
+
+
+base.print()
+
+base.write()
+
+
