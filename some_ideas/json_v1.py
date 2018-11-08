eec32a58543891f485a0fb8472a7a7a5acf27539
varerysan <valery.mosyagin@gmail.com>
2018-11-09 00:10:21 +0300

    Convert from Python data to string and vice versa

--- some_ideas/json_v1.py
+++ some_ideas/json_v1.py
@@ -1,9 +1,13 @@
 import json
 
-print("Hello")
-
 data = {"data": 100, "value": 200}
 
 result = json.dumps(data)
 
-print("result={}=".format(result))
+print("result converted to string={}=".format(result))
+
+input_data = '{"values":[10,20,30],"data": {"name1":123,"name2":"hello"}}'
+
+data2 = json.loads(input_data)
+
+print("python data data2:")
