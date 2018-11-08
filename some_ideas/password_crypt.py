df09d9f0a57cfd69c15e89c8e5ead8c0499f121c
varerysan <valery.mosyagin@gmail.com>
2018-11-08 23:38:09 +0300

    Add encode utf-8 string to bytes. Need for sha256

--- some_ideas/password_crypt.py
+++ some_ideas/password_crypt.py
@@ -1,3 +1,5 @@
+import hashlib
+
 logindata = {"admin": "123", 
              "johh": "PasS",
              "user": "test"}
@@ -5,8 +7,16 @@ logindata = {"admin": "123",
 username = input("Введите имя пользователя:")
 password = input("Введите пароль:")
 
+data = password.encode(encoding="utf-8")
+print("data={}=".format(data))
+
+crypt = hashlib.sha256(data).hexdigest()
+print("Crypt={}=".format(crypt))
+
 correct_pass = logindata.get(username,None)
 if correct_pass == password:
     print("Добро пожаловать")
 else:
     print("Доступ запрещён.")
+
+
