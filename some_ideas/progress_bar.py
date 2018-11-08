76d270fde275036a01556e85714fb0c4dc6070a9
varerysan <valery.mosyagin@gmail.com>
2018-11-09 01:13:56 +0300

    Draft for precise progress bar

--- some_ideas/progress_bar.py
+++ some_ideas/progress_bar.py
@@ -8,7 +8,7 @@ def draw_progress_bar(width, value):
 
     print("[", end="")
     
-    symbol = "\u2592" # 2592, 2593
+    symbol = "\u2588" #"\u2592" # 2592, 2593
     symbol_space = "\u2591"
 
     print( symbol * show_value, end="" )
@@ -17,8 +17,39 @@ def draw_progress_bar(width, value):
     print("]", end="")
     print("  {} %".format(value), end="")
         
+    
+def draw_progress_bar_v2(width, value):
+    
+    show_value = min(width*8, int(8*width * value * 0.01))
+    
+    rest = show_value % 8
+    div = show_value / 8
+    
+    
+
+    #print("[", end="")
+    
+    
+    symbols = [" ", "\u258F", "\u258E", "\u258D", "\u258C", "\u258B", "\u258A", "\u2589", "\u2588"]
+    
+    print(symbols)
+    
+    return
+    
+    symbol = "\u2588" #"\u2592" # 2592, 2593
+    symbol_space = " "
+
+    print( symbol * show_value, end="" )
+    print( symbol_space * (width-show_value), end="")
+    
+    print("]", end="")
+    print("  {} %".format(value), end="")
+        
+
+draw_progress_bar_v2(10,20)
+    
 num = 20
-for n in range(101):
+for n in range(101*0):
     print("\r", end="")
     draw_progress_bar(30,n)
     sleep(0.1)
