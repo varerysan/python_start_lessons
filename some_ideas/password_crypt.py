e333ec5b93af9d311f9d82ab3b11f984ba70d3f6
varerysan <valery.mosyagin@gmail.com>
2018-11-08 23:39:55 +0300

    Comvert text passwords to sha256

--- some_ideas/password_crypt.py
+++ some_ideas/password_crypt.py
@@ -1,8 +1,8 @@
 import hashlib
 
-logindata = {"admin": "123", 
-             "johh": "PasS",
-             "user": "test"}
+logindata = {"admin": "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3", # 123 
+             "johh": "5229375c28a89876d931339bf8849af089c2db709c38b894e91521f930fa2e39", # PasS
+             "user": "9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08"} #test
 
 username = input("Введите имя пользователя:")
 password = input("Введите пароль:")
