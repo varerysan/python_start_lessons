e93a7dd1bcfc750ac71a85b9cd73de5a6e8bfadb
varerysan <valery.mosyagin@gmail.com>
2018-11-10 19:00:22 +0300

    Add create sinus function

--- some_ideas/show_text_graphic.py
+++ some_ideas/show_text_graphic.py
@@ -1,8 +1,30 @@
 #
+import math
+
 
 symbols = [" ","\u2581","\u2582","\u2583","\u2584","\u2585","\u2586","\u2587","\u2588"]
 
 
 def show_vals(data):
-    pass
+    for val in data:
+        print(symbols[val],end="")
+        
+        
+
+def create_sin():
+    data = []
+    for x in range(30):
+        y = int((math.sin(x / 3) + 1)* 7 / 2)
+        if y < 0:
+            y = 0
+        elif y > 7:
+            y = 7
+        data.append(y)
+    return data
+
+data = create_sin()            
+print("data=", data)
+            
+    
+    
 
