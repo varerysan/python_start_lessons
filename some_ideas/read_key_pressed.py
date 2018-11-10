f66f73e2bb3844905eb59175ea4ad34516146d4f
varerysan <valery.mosyagin@gmail.com>
2018-11-10 19:23:46 +0300

    Version 2

--- some_ideas/read_key_pressed.py
+++ some_ideas/read_key_pressed.py
@@ -1,47 +1,45 @@
-import time
-
-
-class _Getch:
-    """Gets a single character from standard input.  Does not echo to the screen."""
-    def __init__(self):
-        try:
-            self.impl = _GetchWindows()
-        except ImportError:
-            self.impl = _GetchUnix()
-
-    def __call__(self): return self.impl()
-    
-class _GetchUnix:
-    def __init__(self):
-        import tty, sys
-
-    def __call__(self):
-        import sys, tty, termios
-        fd = sys.stdin.fileno()
-        old_settings = termios.tcgetattr(fd)
-        try:
-            tty.setraw(sys.stdin.fileno())
-            ch = sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        return ch
-
-class _GetchWindows:
-    def __init__(self):
-        import msvcrt
-
-    def __call__(self):
-        import msvcrt
-        return msvcrt.getch()
-
-
-
-g = _Getch()
-    
-for n in range(100):
-    s = g()
-    if s:
-        print("={}=".format(s), end="")
-    print("-",end="")
-    #time.sleep(0.1)
-    
+#!/usr/bin/python3
+ 
+# adapted from https://github.com/recantha/EduKit3-RC-Keyboard/blob/master/rc_keyboard.py
+ 
+import sys, termios, tty, os, time
+ 
+def getch():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+ 
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+ 
+button_delay = 0.2
+ 
+while True:
+    char = getch()
+ 
+    if (char == "p"):
+        print("Stop!")
+        exit(0)
+ 
+    if (char == "a"):
+        print("Left pressed")
+        time.sleep(button_delay)
+ 
+    elif (char == "d"):
+        print("Right pressed")
+        time.sleep(button_delay)
+ 
+    elif (char == "w"):
+        print("Up pressed")
+        time.sleep(button_delay)
+ 
+    elif (char == "s"):
+        print("Down pressed")
+        time.sleep(button_delay)
+ 
+    elif (char == "1"):
+        print("Number 1 pressed")
+        time.sleep(button_delay)
