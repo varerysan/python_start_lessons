d078b340b1144b6fcc5aa2823d2a185a771a399c
varerysan <valery.mosyagin@gmail.com>
2018-11-13 00:24:27 +0300

    Add draw sereas of circles

--- some_ideas/micro_pseudo_graphics.py
+++ some_ideas/micro_pseudo_graphics.py
@@ -177,7 +177,19 @@ def test_field():
     time.sleep(1)
 
     clear_field(field)
-   
+    
+    xc = int(width/2)
+    yc = int(height/2)
+    for r in range(2, 20):
+        draw_circle(field, width, height, xc, yc, r)
+        time.sleep(0.03)
+        update_field(field, width, height)
+
+    time.sleep(1)
+
+    
+    clear_field(field)
+
     x = 5
     y = 3
     sx = 1
