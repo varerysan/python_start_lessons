f3704e2335412cb41367c4cfe5f2d9d4e93bedda
varerysan <valery.mosyagin@gmail.com>
2018-11-14 23:17:01 +0300

    Test for existing user and get temp range from data

--- some_ideas/swim_water_db.py
+++ some_ideas/swim_water_db.py
@@ -2,3 +2,18 @@
 # Определение подходит ли температура для купания
 # Для каждого пользователя свои предпочтительные границы температур
 
+data = {'John':[15,40],
+        'Anna':[17,38],
+        'Belle':[12,41]}
+
+name = input('Введите своё имя:')
+if name not in data:
+    print("Информации по такому пользователю нет")
+else:    
+    minTemp, maxTemp = data[name]
+    print("minTemp=", minTemp)
+    print("maxTemp=", maxTemp)
+        
+    temp = int(input("Введите температуру воды:"))
+    
+
