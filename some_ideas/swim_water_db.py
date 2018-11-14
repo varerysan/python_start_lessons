fc1b3ad697096037df38d4cd2a12cd0bae6e5791
varerysan <valery.mosyagin@gmail.com>
2018-11-14 23:24:30 +0300

    Add test for temperature

--- some_ideas/swim_water_db.py
+++ some_ideas/swim_water_db.py
@@ -8,12 +8,12 @@ data = {'John':[15,40],
 
 name = input('Введите своё имя:')
 if name not in data:
-    print("Информации по такому пользователю нет")
+    print("Информации по такому пользователю нет.")
 else:    
     minTemp, maxTemp = data[name]
-    print("minTemp=", minTemp)
-    print("maxTemp=", maxTemp)
-        
     temp = int(input("Введите температуру воды:"))
     
-
+    if temp >= minTemp and temp <= maxTemp:
+        print("Хорошая температура воды.")
+    else:
+        print("Температуры воды не очень.")
