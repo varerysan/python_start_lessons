aa9c4198dd393de60b783176fa6d1c4aed33ef11
varerysan <valery.mosyagin@gmail.com>
2018-11-14 23:58:48 +0300

    Add replace example

--- some_ideas/regexp_v1.py
+++ some_ideas/regexp_v1.py
@@ -5,10 +5,19 @@ import re
 
 
 def test1():
+    # find numbers
     text = "lkejrfl49 ljfnekr123erf   115 "
     # text = "dsdfsfsfd"    
     match = re.findall("\d+",text)    
     print(match[0] if match else 'Not found')     
     print(match)
 
+def test2():
+    # replace
+    text = "ljweofitestlefiitestkjweftestkjlf"
+    res = re.sub("test","-TEST-",text)
+    print("res=", res)
+    
+
 test1()
+test2()
