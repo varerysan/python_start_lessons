3385fbeba924a1640cffebcf616dd58d88f7fdcb
varerysan <valery.mosyagin@gmail.com>
2018-11-17 00:22:50 +0300

    Fix error with x+-++x and add raise

--- some_ideas/quadratic_parse.py
+++ some_ideas/quadratic_parse.py
@@ -11,7 +11,10 @@ def parse(equation):
     eq = re.sub(" ", "", equation)
     t1, t2  = re.split("=", eq)
 
-    tokens = re.findall("[-\+]?[^\+-]+", t1)
+#    tokens = re.findall("[-\+]?[^\+-]+", t1)
+    tokens = re.findall("[-\+]?[^\+-]*", t1)
+
+    #print("tokens ===", tokens)
 
     data = []
     for n in tokens:
@@ -42,11 +45,12 @@ def parse_tokens(tokens):
                     dat['var'] = 'x^2'
                 else:
                     dat['error'] = True
+                    raise Exception("BadToken")
         if 'value' not in dat:
             dat['value'] = 1
             
         if 'var' not in dat:
-            dat['var'] = 1
+            dat['var'] = '1'
             
         if dat['sign'] == '-':
             dat['value'] *= -1
@@ -56,31 +60,44 @@ def parse_tokens(tokens):
         data.append(dat)        
     return data
 
-    
+
+def sum_tokens(tokens):
+    data = { 'x^2': 0, 'x': 0, '1': 0}
+    for t in tokens:  
+        data[t['var']] += t['value']     
+    return data
                     
 
 def test(text):
-    print("\n\n------ Example -------")
-    print("eq[{}]\n".format(text))
-    tokens = parse(text)
-    print("tokens=", tokens)
-    data = parse_tokens(tokens)
     
-    print("------ parsed tokens -------")
-    for d in data:
-        print("d=", d)
+    try:
+        print("\n\n------ Example -------")
+        print("eq[{}]\n".format(text))
+        tokens = parse(text)
+        print("tokens=", tokens)
+        data = parse_tokens(tokens)
+        
+        print("------ parsed tokens -------")
+        for d in data:
+            print("d=", d)
+            
+        summa = sum_tokens(data)
+        
+        print("summa=", summa)
+    except:
+        print("Error bad token")
 
 
 test(" 10*x^2 + 20*x - 7 = 0")
 test("-x^2-25=0")
 #test("5*x*x - 10*x + 5 = 0")
 test("7.2*x^2 - 10.15*x + 1.5 = 0")
-#test("x+x-x+x^2-10*x^2=0")
+test("x+x-x+x^2-10*x^2=0")
 #test("x=0")
 #test("5=7")
-#test("A*x=0")
+test("A*x=0")
 #test("10.5*x^3-12=0")
 
-#test("x+-+x=10")
+test("x+-+x=10")
 
 
