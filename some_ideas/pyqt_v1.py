ad8cea9f873f67ecb1342c03547f96a2cb164d48
varerysan <valery.mosyagin@gmail.com>
2018-11-17 22:59:13 +0300

    Add create button

--- some_ideas/pyqt_v1.py
+++ some_ideas/pyqt_v1.py
@@ -1,5 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
+
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import pyqtSlot
+
 
 
 if __name__ == '__main__':
@@ -7,6 +14,11 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     w = QWidget()
+    
+    button = QPushButton('PyQt5 button', w)
+    button.setToolTip('This is an example button')
+    button.move(100,70)
+    
     w.resize(250, 150)
     w.move(300, 300)
     w.setWindowTitle('Simple')
