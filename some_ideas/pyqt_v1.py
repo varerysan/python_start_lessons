daf6b25d01f33c37a4d2b47cb87cd1a8b6cd5884
varerysan <valery.mosyagin@gmail.com>
2018-11-18 23:57:47 +0300

    Add label

--- some_ideas/pyqt_v1.py
+++ some_ideas/pyqt_v1.py
@@ -3,31 +3,46 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget
-
-from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtWidgets import QPushButton, QLabel
 from PyQt5.QtCore import pyqtSlot
 
 
-@pyqtSlot()
-def on_click(self):
-    print('PyQt5 button click')
-
-
+class MyWidget(QWidget):
+    
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 button example '
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 200
+        self.initUI()
+ 
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+ 
+        button = QPushButton('PyQt5 button', self)
+        button.setToolTip('This is an example button')
+        button.move(100,70) 
+        button.clicked.connect(self.on_click)
+        
+        self._label = QLabel("Label-text", self)
+        self._label.move(100,150)
+        
+ 
+        self.show()
+ 
+    @pyqtSlot()
+    def on_click(self):
+        print('PyQt5 button click. Test')
+        self._label.setText("Hello")
+        self._label.adjustSize()
+        self._label.show()
+        
+ 
 if __name__ == '__main__':
-
     app = QApplication(sys.argv)
-
-    w = QWidget()
+    ex = MyWidget()
+    sys.exit(app.exec_())    
     
-    button = QPushButton('PyQt5 button', w)
-    button.setToolTip('This is an example button')
-    button.move(100,70)
-    
-    button.clicked.connect(on_click)
-    
-    w.resize(250, 150)
-    w.move(300, 300)
-    w.setWindowTitle('Simple. Test' )
-    w.show()
-
-    sys.exit(app.exec_())
