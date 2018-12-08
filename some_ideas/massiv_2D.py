649f9dbf26bb85d5b1fca3a5e543225a6eed5f60
varerysan <valery.mosyagin@gmail.com>
2018-12-09 00:12:15 +0300

    Some list sizes

--- some_ideas/massiv_2D.py
+++ some_ideas/massiv_2D.py
@@ -1,4 +1,5 @@
-
+# example how to create 2D array
+import sys
 data = [ [0 for x in range(5) ] for k in range(3)]
 
 print("data=", data)
@@ -14,4 +15,11 @@ for y in range(3):
         print("{:4}".format(data[y][x]), end="")
     print()
 
+d1 = [ x/10.0 for x in range(10000) ]
+print("d1 ( 10000 )size=", sys.getsizeof(d1))
 
+d2 = [ x/10.0 for x in range(100000) ]
+print("d2 ( 100000 )size=", sys.getsizeof(d2))
+
+d3 = [ x/10.0 for x in range(1000000) ]
+print("d3 ( 1000000 )size=", sys.getsizeof(d3))
