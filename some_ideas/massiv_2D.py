304ad777c75b7bb86740abbeaf549f93ff71ede0
varerysan <valery.mosyagin@gmail.com>
2018-12-08 23:32:39 +0300

    2D data

---
+++ some_ideas/massiv_2D.py
@@ -0,0 +1,17 @@
+
+data = [ [0 for x in range(5) ] for k in range(3)]
+
+print("data=", data)
+
+data[0][0] = 100
+data[1][0] = 200
+
+print("data=", data)
+
+print("----------------")
+for y in range(3):
+    for x in range(5):
+        print("{:4}".format(data[y][x]), end="")
+    print()
+
+
