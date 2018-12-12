500e718c907ca0737cfc55d488f26e063202dfbf
varerysan <valery.mosyagin@gmail.com>
2018-12-13 00:40:17 +0300

    All

---
+++ some_ideas/prime_numbers_v2.py
@@ -0,0 +1,9 @@
+from math import sqrt
+
+def is_prime(num):
+    return all( num % n != 0 for n in range(2, int(sqrt(num))) )
+
+
+primes = [x for x in range(2,1001) if is_prime(x)]
+print(primes)
+
