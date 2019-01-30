29414119ce3c913af867a32816cce804ef8839b9
Valery Mosyagin <valery.mosyagin@gmail.com>
2019-01-30 16:19:28 +0300

    Number factorization

---
+++ some_ideas/factorize.py
@@ -0,0 +1,28 @@
+n = int(input("Number"))
+
+divs = {}
+
+
+for k in range(2,n+1):
+    while n % k == 0:
+        divs[k] = divs.get(k,0) + 1
+        n = n / k
+
+    if n == 1:
+        break
+
+full = []
+for k,v in divs.items():
+    
+    kk = [str(k)] * v
+    bb = "*".join(kk)
+    #print( bb )
+    full.append(bb)
+
+
+print( "*".join(full) )
+
+
+
+#print(full)
+          
---
+++ some_ideas/factorize_v2.py
@@ -0,0 +1,14 @@
+n = int(input("Number"))
+
+
+vals = []
+
+for k in range(2,n+1):
+    while n % k == 0:
+        vals.append(str(k))
+        n = n / k
+
+    if n == 1:
+        break
+
+print( "*".join(vals) )
